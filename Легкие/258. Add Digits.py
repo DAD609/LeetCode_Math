c1b0666d1584854cb22fec4a6565a53cd50e7ad0
Almaz <67281187+DAD609@users.noreply.github.com>
2024-05-28 17:21:01 +0300

    Create 258. Add Digits.py

---
+++ Легкие/258. Add Digits.py
@@ -0,0 +1,8 @@
+'''
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+'''
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
